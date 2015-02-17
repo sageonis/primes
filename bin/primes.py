1205311785da553f7d8d78e4b54f96a9ce1a4915
Sage <sageonis@gmail.com>
2015-02-16 22:50:48 -0500

    Added .env settings so that users can configure project defaults.

--- bin/primes.py
+++ bin/primes.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
+import os
 import sys
 import time
+import dotenv
 import argparse
+
+dotenv.read_dotenv(os.path.dirname(os.path.realpath(__file__)) + '/../.env')
+
+if os.environ['START']:
+    START = int(os.environ['START'])
+else:
+    START = 2
+
+if os.environ['FINISH']:
+    FINISH = int(os.environ['FINISH'])
+else:
+    FINISH = 1000
+
+if os.environ['HOW_MANY']:
+    HOW_MANY = int(os.environ['HOW_MANY'])
+else:
+    HOW_MANY = None
+
+if os.environ['COLUMN_PADDING']:
+    COLUMN_PADDING = int(os.environ['COLUMN_PADDING'])
+else:
+    COLUMN_PADDING = 2
+
+if os.environ['HOW_MANY_PER_ROW']:
+    HOW_MANY_PER_ROW = int(os.environ['HOW_MANY_PER_ROW'])
+else:
+    HOW_MANY_PER_ROW = 15
 
 
 class Primes(object):
@@ -9,13 +38,13 @@ class Primes(object):
     def __init__(self, start, howmany, finish):
         self.start_time = time.time()
 
-        if not start == None and start < 2:
+        if not start == None and start < START:
             sys.exit("If passing start value, must be >= first prime number of 2.")
 
         if not start == None:
             self.start = start
         else:
-            self.start = 2
+            self.start = START
 
         if not howmany == None and howmany < 1:
             sys.exit("If passing howmany value, must be >= 1.")
@@ -23,9 +52,9 @@ class Primes(object):
         if not howmany == None and howmany > 0:
             self.howmany = howmany
         else:
-            self.howmany = None
+            self.howmany = HOW_MANY
 
-        if not finish == None and finish < 2:
+        if not finish == None and finish < START:
             sys.exit("If passing finish value, must be >= first prime number of 2.")
 
         if not finish == None:
@@ -34,7 +63,7 @@ class Primes(object):
             if not howmany == None:
                 self.finish = None
             else:
-                self.finish = 1000
+                self.finish = FINISH
 
         self.primes = []
         return
@@ -73,12 +102,12 @@ class Primes(object):
 
     def out(self):
         start = 0
-        finish = 15
-        col_width = max(len(str(p)) for p in self.primes) + 2
+        finish = HOW_MANY_PER_ROW
+        col_width = max(len(str(p)) for p in self.primes) + COLUMN_PADDING
         while start < len(self.primes):
             print "".join(str(p).ljust(col_width) for p in self.primes[start:finish])
-            start += 15
-            finish += 15
+            start += HOW_MANY_PER_ROW
+            finish += HOW_MANY_PER_ROW
         return
 
     def get_elapsed(self):
